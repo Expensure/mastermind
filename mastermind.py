3c16294274400fbe2dba10d5d20ccfbc3804e0da
Expensure <jaspervanloon@outlook.com>
2021-02-23 13:09:03 +0100

    Feedback completely revamped

--- mastermind.py
+++ mastermind.py
@@ -38,25 +38,27 @@ def string_to_list(string):
 
 
 def feedback(guess, guess_me):
-    # Kijkt de resultaten van de gok van speler of computer na
-    positionCorrect = 0  # Zwarte pins
-    colorCorrect = 0  # Witte pins
-    wrong = 0
-    temp_guessme = guess_me
-    for color in guess:
-        for p in guess_me:
-            if color == p:
-                colorCorrect += 1
-                temp_guessme.remove(p)
-                break
-            elif color in temp_guessme:
-                positionCorrect += 1
-                temp_guessme.remove(p)
-                break
-            else:
-                wrong += 1
-                break
-    return [colorCorrect,positionCorrect, wrong]
+    blacks = 0
+    whites = 0
+    used = [False, False, False, False]
+
+    # search only blacks and mark as "used" black elements
+    for i in range(4):
+        if guess[i] == guess_me[i]:
+            blacks += 1
+            used[i] = True
+
+    # search only whites (but skip "used" elements)
+    # and mark as "used" every white element in code
+    # so it can't be used twice
+    for i in range(4):  # guess index
+        for j in range(4):  # code index
+            if not used[j] and guess[j] == guess_me[i]:
+                whites += 1
+                used[j] = True
+
+    wrongs = len(guess_me) - (blacks + whites)
+    return blacks, whites, wrongs
 
 
 def guess_self(lst, guess_me, game, attempts):
@@ -73,10 +75,10 @@ def guess_self(lst, guess_me, game, attempts):
                 if guess not in all_combinations:  # Als input niet goed is, geeft aan speler aan dat het fout is
                     print("Foute input, probeer iets zoals 'RGBY' in te vullen")
 
-            correct, existing = feedback(guess, guess_me)  # Laat de gok testen
+            correct, existing, wrong = feedback(guess, guess_me)  # Laat de gok testen
             print("Je hebt ", correct, " correct, en verder heb je", existing, "kleuren")
 
-            if correct == 4:  # Winning condition
+            if correct == len(guess_me):  # Winning condition
                 print('')
                 return 'YOU WIN!, de code was inderdaad', guess, '. Het kostte', attempts, 'pogingen'
 
@@ -103,11 +105,11 @@ def simple_algorithm():
 
     def compare_score(guess, guess_me, all_possibilities):
         new_temp_list = []
-        guess_result = guess_resultblack, guess_resultwhite = feedback(guess, guess_me)
+        guess_result = feedback(guess, guess_me)
         for i in all_possibilities:
-            others_result = feedback(i, guess_me)
+            print (guess)
             print(i)
-            print(guess_result)
+            others_result = feedback(i, guess_me)
             print(others_result)
             if others_result == guess_result:
                 new_temp_list.append(i)
@@ -126,7 +128,7 @@ def simple_algorithm():
             return results(guess, tries, all_possibilities)
 
     result = results('AAAA', 0, all_possibilities)
-    print(result)
+    return result
 
 
 kleur_div = int(input("Uit hoeveel kleuren kan er worden gekozen? Maximaal 10 invullen a.u.b "))
